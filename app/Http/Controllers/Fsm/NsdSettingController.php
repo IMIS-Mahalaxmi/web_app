919854191c0aca189ede0721af9a0e87bd5a59e9
Suprit Shrestha <124333491+suprit1234@users.noreply.github.com>
2025-04-28 14:39:11 +0545

    Added NSD Setting
    NSD Setting used for saving credentials for the admin

---
+++ app/Http/Controllers/Fsm/NsdSettingController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Http\Controllers\Fsm;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Fsm\NSD;
+use Illuminate\Support\Facades\Crypt;
+use App\Http\Controllers\Fsm\NsdDashboardController;
+
+class NsdSettingController extends Controller
+{
+    public function index()
+    {
+        $nsd = NSD::first();
+        if (!$nsd) {
+            return redirect()->route('nsd-setting.create');
+        }
+        return redirect()->route('nsd-setting.edit', $nsd->id);
+    }
+
+    public function create()
+    {   
+        $page_title = 'National Sanitation Dashboard (NSD) Credentials';
+        return view('fsm.nsd-setting.create', compact('page_title'));
+    }
+
+    public function edit($id)
+    {
+        $page_title = 'National Sanitation Dashboard (NSD) Credentials';
+        $nsd= NSD::findOrFail($id);
+        return view('fsm.nsd-setting.edit', compact('nsd', 'page_title'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'nsd_username' => 'required|string',
+            'nsd_password' => 'nullable|string',
+            'city' => 'required|string',
+            'api_post_url' => 'required|url',
+            'api_login_url' => 'required|url',
+        ]);
+
+        $nsd = NSD::create([
+            'nsd_username'   => $data['nsd_username'],
+            'nsd_password'   => !empty($data['nsd_password']) ? Crypt::encrypt($data['nsd_password']) : null,
+            'city'           => $data['city'],
+            'api_post_url'   => $data['api_post_url'],
+            'api_login_url'  => $data['api_login_url'],
+        ]);
+
+        $credentials = [
+            'nsd_username' => $data['nsd_username'],
+            'nsd_password' => !empty($data['nsd_password']) ? $data['nsd_password'] : Crypt::decrypt($nsd->nsd_password),
+            'api_login_url' => $data['api_login_url'],
+        ];  
+        
+        $dashboardController = new NsdDashboardController();
+        $bearerToken = $dashboardController->getBearerToken($credentials);
+
+        if (!$bearerToken) {
+            return redirect()->back()
+                ->withInput($request->all())
+                ->with('error', 'Invalid credentials. Bearer token verification failed.');
+        }
+
+
+        $city = $data['city'];
+        $checkStatus = $dashboardController->checkNsdStatus($city);
+
+        if (!$checkStatus) {
+            return redirect()->back()
+                ->withInput($request->all())
+                ->with('error', 'NSD status check failed. Unable to verify the NSD status.');
+        }
+
+        // Save the data if both checks pass
+        $nsd->nsd_username = $data['nsd_username'];
+        if (!empty($data['nsd_password'])) {
+            $nsd->nsd_password = Crypt::encrypt($data['nsd_password']);
+        }
+        $nsd->city = $data['city'];
+        $nsd->api_post_url = $data['api_post_url'];
+        $nsd->api_login_url = $data['api_login_url'];
+        $nsd->save();
+
+        return redirect()->route('nsd-setting.edit', $nsd->id)->with('success', 'National Sanitation Dashboard Credentials created successfully');
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        $data = $request->validate([
+            'nsd_username' => 'required|string',
+            'nsd_password' => 'nullable|string',
+            'city'         => 'required|string',
+            'api_post_url' => 'required|url',
+            'api_login_url' => 'required|url',
+        ]);
+
+        $nsd = NSD::findOrFail($id);
+
+        $credentials = [
+            'nsd_username' => $data['nsd_username'],
+            'nsd_password' => !empty($data['nsd_password']) ? $data['nsd_password'] : Crypt::decrypt($nsd->nsd_password),
+        ];
+
+        $dashboardController = new NsdDashboardController();
+        $bearerToken = $dashboardController->getBearerToken($credentials);
+        
+        if (!$bearerToken) {
+            return redirect()->back()
+                ->withInput($request->all())
+                ->with('error', 'Invalid credentials. Bearer token verification failed.');
+        }
+
+        $city = $data['city'];
+        $checkStatus = $dashboardController->checkNsdStatus($city);
+      
+        if (!$checkStatus) {
+            return redirect()->back()
+                ->withInput($request->all())
+                ->with('error', 'NSD status check failed. Unable to verify the NSD status.');
+        }
+       
+        // Save the data if both checks pass
+        $nsd->nsd_username = $data['nsd_username'];
+        if (!empty($data['nsd_password'])) {
+            $nsd->nsd_password = Crypt::encrypt($data['nsd_password']);
+        }
+        $nsd->city = $data['city'];
+        $nsd->api_post_url = $data['api_post_url'];
+        $nsd->api_login_url = $data['api_login_url'];
+        $nsd->save();
+
+        return redirect()->route('nsd-setting.edit', $id)
+            ->with('success', 'National Sanitation Dashboard settings updated successfully');
+        }
+       
+}
